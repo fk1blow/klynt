59c4dc0a4e51a4b8e262f6c34a12ff816d5dfa1d
Dragos Tudorache <hedphug@gmail.com>
2016-01-07 01:33:25 +0200

    refactor handler function arity match

--- lib/klynt/handler.ex
+++ lib/klynt/handler.ex
@@ -7,15 +7,12 @@ defmodule KL.Handler do
   defmodule Model do
     alias KL.Model.Response
 
-    def transform(%Response{content: content, error: nil, meta: meta}, model) do
+    def transform(%Response{content: content, error: nil}, model) do
       case Code.eval_quoted(model) do
         {nil, _}   -> content
         {model, _} -> struct(model, content.data)
       end
     end
-
-    def transform(%Response{content: nil, error: error}, _model) do
-      error
-    end
+    def transform(%Response{content: nil, error: error}, _), do: error
   end
 end
